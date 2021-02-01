8ea4c33c72c86c1cf125ccc25ee4a7a10ce0732b
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-01-31 23:49:29 -0500

    added new

---
+++ playground/hr/randomString.cpp
@@ -0,0 +1,26 @@
+// run in gcc with: gcc randomString.cpp -lstdc++
+// then run: a.exe
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int i , n;
+	char * buffer;
+	
+	printf("How long do you want the string? ");
+	scanf("%d", &i);
+	
+	buffer = (char*) malloc (i+1);
+	if(buffer == NULL) exit (1);
+	
+	for(n=0; n<1; n++)
+		buffer[n]=rand()%26+'a';
+	buffer[i]='\0';
+	
+	printf("Random string: %s\n", buffer);
+	free (buffer);
+
+	return 0;
+}
